013624b65953dc0966e4a72804d677dc6be24421
sanjsanj <sanjsanj@hotmail.com>
2015-03-03 13:22:30 +0000

    First commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,27 @@
+#def fizz(number)
+#  if number % 3 == 0
+#    puts "fizz"
+#  end
+#end
+#
+#def buzz(number)
+#  if number % 5 == 0
+#    puts "buzz"
+#  end
+#end
+
+def whatever(number)
+  if number % 15 == 0
+    ####
+  elsif number % 5 == 0
+    ####
+  elsif number % 3 == 0
+    ####
+  else
+    ####
+  end
+end
+
+fizz(3)
+fizz(5)
+fizz(9)
