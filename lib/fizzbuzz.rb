08a7726d64599013bd8ed14e2df444131bc2e8c7
sanjsanj <sanjsanj@hotmail.com>
2015-03-11 14:01:34 +0000

    Complete fizzbuzz TDD challenge

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,22 @@
+def fizzbuzz(number)
+  return "FizzBuzz" if is_divisible_by_fifteen?(number)
+  return "Fizz" if is_divisible_by_three?(number)
+  return "Buzz" if is_divisible_by_five?(number)
+  number
+end
+
 def is_divisible_by_three?(number)
-  true
+  is_divisible_by?(number, 3)
+end
+
+def is_divisible_by_five?(number)
+  is_divisible_by?(number, 5)
+end
+
+def is_divisible_by_fifteen?(number)
+  is_divisible_by?(number, 15)
+end
+
+def is_divisible_by?(number, divisor)
+  number % divisor == 0
 end
